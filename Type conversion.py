23cddc7826bc507692ec6c04315126794a5520ac
Aswin <aswinrnair.12@gmail.com>
2025-12-30 15:37:06 +0530

    Class work shop.py

---
+++ Type conversion.py
@@ -0,0 +1,14 @@
+ajuice = 15.5
+ojuice = 20
+gjuice = 10.25
+
+total = ajuice + ojuice + gjuice
+
+print("Total volume sold=",total)
+
+print("Total volume to integer: ",int(total))
+
+print("Total volume to a string:", str(total))
+
+import random
+print(random.randrange(5,10)+total)
