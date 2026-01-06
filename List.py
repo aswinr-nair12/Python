a3064256a7ca654a1ba99dcbf4e85e4dae00dbb5
Aswin <aswinrnair.12@gmail.com>
2026-01-06 14:23:36 +0530

    loop

---
+++ List.py
@@ -0,0 +1,13 @@
+fruits = ["Apple", "Orange", "Grapes", "Pineapple"]
+vegetables = ["Carrot", "Onion", "Potato", "Spinach"]
+beverages = ["Water", "Mango Juice", "Shakes"]
+
+fruits.append("Avacado")
+print(fruits)
+
+vegetables.insert(2, "Brinjal")
+print(vegetables)
+
+beverages.pop()
+print(beverages)
+
